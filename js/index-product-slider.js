98e5071da10d431409effca34d8f6a264e008855
Sue Wong <138675776+suengit6@users.noreply.github.com>
2023-08-14 18:06:35 +0800

    Add files via upload

--- js/index-product-slider.js
+++ js/index-product-slider.js
@@ -1,37 +1,37 @@
 const products = [
 	{
 		name: 'Product 1',
-		price: 21,
+		price: '$21',
 		description: 'This is the description for Product 1',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/03b265a1b02fef5303267a32f751be488c177ed0/products/img_product_01.jpeg?raw=true',
 	},
 	{
 		name: 'Product 2',
-		price: 31,
+		price: '$31',
 		description: 'This is the description for Product 2',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/03b265a1b02fef5303267a32f751be488c177ed0/products/img_product_02.jpeg?raw=true',
 	},
 	{
 		name: 'Product 3',
-		price: 55,
+		price: '$55',
 		description: 'This is the description for Product 3',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/03b265a1b02fef5303267a32f751be488c177ed0/products/img_product_03.jpeg?raw=true',
 	},
 	{
 		name: 'Product 4',
-		price: 34,
+		price: '$34',
 		description: 'This is the description for Product 4',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/main/35D1F08F-F01A-4488-8E35-2C960A8181A4.jpeg?raw=true',
 	},
 	{
 		name: 'Product 5',
-		price: 23,
+		price: '$23',
 		description: 'This is the description for Product 5',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/main/7BEC8DCC-C217-4E4D-A515-AD64279011CA.jpeg?raw=true',
 	},
 	{
 		name: 'Product 6',
-		price: 68,
+		price: '$68',
 		description: 'This is the description for Product 6',
 		imageUrl: 'https://github.com/suengit6/activity-images/blob/main/76403267-333A-4EFF-A9BF-4889E6A2A4F0.jpeg?raw=true',
 	}
